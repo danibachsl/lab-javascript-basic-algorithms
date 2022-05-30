30a1e2d127850398d83e2231d3c9b7d22ff64545
Dani Bachs <danibachsl@protonmail.com>
2022-05-30 22:46:26 +0200

    Just finished Bonus part

--- js/index.js
+++ js/index.js
@@ -53,4 +53,18 @@ if ((hacker1.localeCompare(hacker2)) === 1) {
     console.log(`What?! You both have the same name?`);
 }
 
-//
+// BONUS TIME!
+// Bonus 1
+
+let paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at facilisis nunc. Vestibulum sed est in dui lacinia condimentum sit amet tempus leo. Nulla sit amet semper justo, at ullamcorper sapien. Cras fringilla bibendum finibus. Ut vel sollicitudin dolor. Aenean elit erat, iaculis ac mattis eget, ultricies at nibh. Nullam sed accumsan velit, at consequat nisi. Sed scelerisque commodo neque nec hendrerit. /nIn vitae porta leo. Cras pulvinar et neque ac imperdiet. Vivamus consectetur pretium lacus. Vivamus eget mattis mauris. Praesent pellentesque tristique pharetra. Praesent vulputate blandit tortor, at tincidunt justo varius et. Ut ut lectus dui. In quam turpis, pellentesque ut urna sed, mollis tincidunt est. Mauris accumsan lectus massa, nec sodales ante dapibus vel. Quisque enim diam, hendrerit in tristique sed, dignissim in arcu. Maecenas vulputate gravida rhoncus. Pellentesque rutrum diam quis eros tempor interdum. Curabitur id est porta, feugiat quam in, mollis nisi. Suspendisse vehicula ex sit amet gravida imperdiet. Morbi viverra ut turpis vel finibus. Curabitur quis cursus neque, ac consectetur est. /nCras ut est id quam finibus posuere non at est. Vestibulum nec ipsum a ante molestie pellentesque vitae ac sem. Curabitur vel urna tincidunt, blandit lorem id, iaculis orci. Maecenas ullamcorper vulputate metus, et pulvinar tortor scelerisque vitae. Duis quis condimentum magna, at malesuada urna. Suspendisse potenti. Mauris pretium condimentum nibh, non scelerisque nibh dictum nec. Nulla eleifend eleifend semper.';
+
+function WordCount(str) { 
+    return str.split(" ").length;
+}
+
+console.log(WordCount(paragraph));
+
+// Bonus 2
+
+let phraseToCheck = 'Amor, Roma';
+
